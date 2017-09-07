3fd73909eef315ca189b416e9ec2e6bb6892a445
aminmahrami <31386996+aminmahrami@users.noreply.github.com>
2017-09-07 21:58:02 +0200

    function that runs a function.

--- javascript/week1/index13.js
+++ javascript/week1/index13.js
@@ -1,4 +1,6 @@
-    function foo(func) {
+/* 14. We saw that we can pass functions as arguments to other functions.
+Your task is to write a function that takes another function as an argument and runs it. */ 
+function foo(func) {
         func();
     }
     
