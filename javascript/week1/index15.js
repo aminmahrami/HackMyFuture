6a6b9662f1d34bd3e85d3f8a39914187a8a371a8
aminmahrami <31386996+aminmahrami@users.noreply.github.com>
2017-09-07 22:09:38 +0200

    Comparing object

--- javascript/week1/index15.js
+++ javascript/week1/index15.js
@@ -1,8 +1,10 @@
-     var o1 = { foo: 'bar' };
-     var o2 = { foo: 'bar' };
-     var o3 = o2;
+ //16  
+var o1 = { foo: 'bar' };
+var o2 = { foo: 'bar' };
+var o3 = o2;
 o2.foo="cat";
 o1=o2;
 console.log(o2);
 console.log(o3);
 console.log(o1);
+//order is important
