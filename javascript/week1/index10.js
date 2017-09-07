9e81340c6669739f1530e3c8418321cbff352e68
aminmahrami <31386996+aminmahrami@users.noreply.github.com>
2017-09-07 21:53:40 +0200

    vehicls list

--- javascript/week1/index10.js
+++ javascript/week1/index10.js
@@ -1,3 +1,6 @@
+/*11.What if you add one more vehicle to the list,
+can you have that added to the advertisement without changing the code for question 7?*/
+
 let vehicles = ["motorbike", "caravan", "bike"];
 
 vehicles.push("somthing");
