59549df75f74758dbca26f02fee7f0e4535134c0
aminmahrami <31386996+aminmahrami@users.noreply.github.com>
2017-09-07 22:18:19 +0200

    function that prints color and vehicle type

--- javascript/week1/index3.js
+++ javascript/week1/index3.js
@@ -1,3 +1,5 @@
+/* 4 . Create a function named vehicleType that receives a color, and a code, 1 for car, 2 for motorbike.
+And prints "a blue motorbike" for example when called as vehicleType("blue", 2)*/
 function vehicleType(x,y){
   if (y == 1){
     console.log('a '+x+' car');
