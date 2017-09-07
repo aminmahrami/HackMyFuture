92ccacf9bf8c7b582024f17ad4be86e9d6ca1b70
aminmahrami <31386996+aminmahrami@users.noreply.github.com>
2017-09-07 21:44:27 +0200

    prints out, "a red car"

--- javascript/week1/index1.js
+++ javascript/week1/index1.js
@@ -1,3 +1,5 @@
+/*Create a function named colorCar that receives a color,
+and prints out, "a red car" for example. (Hint: put it in your file and run it like before.)*/
 function colorCar(x){
   return 'a '+x+' car';
 }
