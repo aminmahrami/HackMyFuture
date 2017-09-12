3d9bf2a149187249283ad6aee2b5b6a47774f6a6
aminmahrami <31386996+aminmahrami@users.noreply.github.com>
2017-09-12 21:13:11 +0200

    Update hw2nd.js

--- hw2nd.js
+++ hw2nd.js
@@ -11,8 +11,12 @@ console.log(colors.length); // length = 3
 //checks the types of two variables
 let x = 9;
 let y = 'Hello';
-if(x==y)(console.log('SAME TYPE'));
-if(x!=y)(console.log('NOT SAME TYPE'));
+if(x==y){
+  (console.log('SAME TYPE'))
+};
+if(x!=y){
+  (console.log('NOT SAME TYPE'))
+};
 
 let items = ["soap","gel",5,"blade"]; //multiple types in an array
 
